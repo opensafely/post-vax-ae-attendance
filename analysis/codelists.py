c9141d2c5fe0328e3e92acbc3bed26914dba49df
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-08-25 10:36:55 +0100

    add primis codelists

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -104,4 +104,197 @@ nonshield = codelist_from_csv(
 
 
 
+# Asthma Diagnosis code
+ast = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-ast.csv",
+  system="snomed",
+  column="code",
+)
+
+# Asthma Admission codes
+astadm = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-astadm.csv",
+  system="snomed",
+  column="code",
+)
+
+# Asthma systemic steroid prescription codes
+astrx = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-astrx.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic Respiratory Disease
+resp_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-resp_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic heart disease codes
+chd_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-chd_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic kidney disease diagnostic codes
+ckd_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-ckd_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic kidney disease codes - all stages
+ckd15 = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-ckd15.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic kidney disease codes-stages 3 - 5
+ckd35 = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-ckd35.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic Liver disease codes
+cld = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-cld.csv",
+  system="snomed",
+  column="code",
+)
+
+# Diabetes diagnosis codes
+diab = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-diab.csv",
+  system="snomed",
+  column="code",
+)
+
+# Immunosuppression diagnosis codes
+immdx_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-immdx_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Immunosuppression medication codes
+immrx = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-immrx.csv",
+  system="snomed",
+  column="code",
+)
+
+# Chronic Neurological Disease including Significant Learning Disorder
+cns_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-cns_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# Asplenia or Dysfunction of the Spleen codes
+spln_cov = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-spln_cov.csv",
+  system="snomed",
+  column="code",
+)
+
+# BMI
+bmi = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-bmi.csv",
+  system="snomed",
+  column="code",
+)
+
+# All BMI coded terms
+bmi_stage = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-bmi_stage.csv",
+  system="snomed",
+  column="code",
+)
+
+# Severe Obesity code recorded
+sev_obesity = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-sev_obesity.csv",
+  system="snomed",
+  column="code",
+)
+
+# Diabetes resolved codes
+dmres = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-dmres.csv",
+  system="snomed",
+  column="code",
+)
+
+# Severe Mental Illness codes
+sev_mental = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-sev_mental.csv",
+  system="snomed",
+  column="code",
+)
+
+# Remission codes relating to Severe Mental Illness
+smhres = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-smhres.csv",
+  system="snomed",
+  column="code",
+)
+
+
+# to represent household contact of shielding individual
+hhld_imdef = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-hhld_imdef.csv",
+  system="snomed",
+  column="code",
+)
+
+# Wider Learning Disability
+learndis = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-learndis.csv",
+  system="snomed",
+  column="code",
+)
+
+
+
+# Carer codes
+carer = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-carer.csv",
+  system="snomed",
+  column="code",
+)
+
+# No longer a carer codes
+notcarer = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-notcarer.csv",
+  system="snomed",
+  column="code",
+)
+
+# Employed by Care Home codes
+carehomeemployee = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-carehome.csv",
+  system="snomed",
+  column="code",
+)
+
+# Employed by nursing home codes
+nursehomeemployee = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-nursehome.csv",
+  system="snomed",
+  column="code",
+)
+
+# Employed by domiciliary care provider codes
+domcareemployee = codelist_from_csv(
+  "codelists/primis-covid19-vacc-uptake-domcare.csv",
+  system="snomed",
+  column="code",
+)
+
+
 
