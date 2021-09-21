52e58d912b68bdc4d52ea9c4ddf8a1aa278ca514
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-09-21 13:46:30 +0100

    use new product name for pfizer

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -117,7 +117,7 @@ study = StudyDefinition(
   #################################################################
   
   covid_vax_pfizer_1_date=patients.with_tpp_vaccination_record(
-    product_name_matches="COVID-19 mRNA Vaccine Pfizer-BioNTech BNT162b2 30micrograms/0.3ml dose conc for susp for inj MDV",
+    product_name_matches="COVID-19 mRNA Vaccine Comirnaty 30micrograms/0.3ml dose conc for susp for inj MDV (Pfizer)",
     find_first_match_in_period=True,
     returning="date",
     date_format="YYYY-MM-DD",
@@ -132,7 +132,7 @@ study = StudyDefinition(
   
   
   covid_vax_pfizer_2_date=patients.with_tpp_vaccination_record(
-    product_name_matches="COVID-19 mRNA Vaccine Pfizer-BioNTech BNT162b2 30micrograms/0.3ml dose conc for susp for inj MDV",
+    product_name_matches="COVID-19 mRNA Vaccine Comirnaty 30micrograms/0.3ml dose conc for susp for inj MDV (Pfizer)",
     on_or_after="covid_vax_pfizer_1_date + 1 days",
     find_first_match_in_period=True,
     returning="date",
@@ -147,7 +147,7 @@ study = StudyDefinition(
   ),
   
   covid_vax_pfizer_3_date=patients.with_tpp_vaccination_record(
-    product_name_matches="COVID-19 mRNA Vaccine Pfizer-BioNTech BNT162b2 30micrograms/0.3ml dose conc for susp for inj MDV",
+    product_name_matches="COVID-19 mRNA Vaccine Comirnaty 30micrograms/0.3ml dose conc for susp for inj MDV (Pfizer)",
     on_or_after="covid_vax_pfizer_2_date + 1 days",
     find_first_match_in_period=True,
     returning="date",
